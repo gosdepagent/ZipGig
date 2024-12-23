bf5598c895e7462ee430fe08739c2cf74807c819
Yana V <velikanova6277@gmail.com>
2024-12-22 19:40:30 -0500

    Initial Commit

---
+++ ZipGig/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZipGig
+//
+//  Created by Yanye Velikanova on 12/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZipGig/ZipGigApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZipGigApp.swift
+//  ZipGig
+//
+//  Created by Yanye Velikanova on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ZipGigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
